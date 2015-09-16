d6c09c30c5abf15168fcd3b28a60546c5ab7f02f
John Walters <johnmwalters@gmail.com>
2015-09-16 13:14:34 -0400

    Update q6_strings.py
    Updated with answers

--- python/q6_strings.py
+++ python/q6_strings.py
@@ -3,118 +3,166 @@
 
 
 def donuts(count):
+    if count >= 10:
+        print 'Number of donuts: many'
+    else:
+        print 'Number of donuts: ' + str(count)
+
     """
     Given an int count of a number of donuts, return a string of the
     form 'Number of donuts: <count>', where <count> is the number
     passed in. However, if the count is 10 or more, then use the word
     'many' instead of the actual count.
-
-    >>> donuts(4)
-    'Number of donuts: 4'
-    >>> donuts(9)
-    'Number of donuts: 9'
-    >>> donuts(10)
-    'Number of donuts: many'
-    >>> donuts(99)
-    'Number of donuts: many'
     """
-    raise NotImplementedError
 
+donuts(4)
+
+donuts(9)
+
+donuts(10)
+
+donuts(99)
 
 def both_ends(s):
+    if len(s) < 2:
+        print ''
+    else:
+        print s[:2] + s[-2:]
+
     """
     Given a string s, return a string made of the first 2 and the last
     2 chars of the original string, so 'spring' yields 'spng'.
     However, if the string length is less than 2, return instead the
     empty string.
-
-    >>> both_ends('spring')
-    'spng'
-    >>> both_ends('Hello')
-    'Helo'
-    >>> both_ends('a')
-    ''
-    >>> both_ends('xyz')
-    'xyyz'
     """
-    raise NotImplementedError
+
+both_ends('spring')
+
+both_ends('Hello')
+
+both_ends('a')
+
+both_ends('xyz')
 
 
 def fix_start(s):
-    """
-    Given a string s, return a string where all occurences of its
-    first char have been changed to '*', except do not change the
-    first char itself. e.g. 'babble' yields 'ba**le' Assume that the
-    string is length 1 or more.
+    first_char = s[:1]
+    length = len(s)
+    remaining_char = s[(-length+1):]
+    temp_find = remaining_char.find(first_char)
+    if temp_find == -1:
+        print s
+    else:
+        print first_char + remaining_char.replace(first_char, '*')
 
-    >>> fix_start('babble')
-    'ba**le'
-    >>> fix_start('aardvark')
-    'a*rdv*rk'
-    >>> fix_start('google')
-    'goo*le'
-    >>> fix_start('donut')
-    'donut'
     """
-    raise NotImplementedError
+    #Given a string s, return a string where all occurences of its
+    #first char have been changed to '*', except do not change the
+    #first char itself. e.g. 'babble' yields 'ba**le' Assume that the
+    #string is length 1 or more.
+    """
+
+fix_start('babble')
+
+fix_start('aardvark')
+
+fix_start('google')
+
+fix_start('donut')
 
 
 def mix_up(a, b):
+    first_a = a[:2]
+    first_b = b[:2]
+    remaining_a = a[-len(a)+2:] 
+    remaining_b = b[-len(b)+2:]
+    new_a = first_b + remaining_a
+    new_b = first_a + remaining_b
+    print new_a + " " + new_b
+    
     """
-    Given strings a and b, return a single string with a and b
-    separated by a space '<a> <b>', except swap the first 2 chars of
-    each string. Assume a and b are length 2 or more.
+    #Given strings a and b, return a single string with a and b
+    #separated by a space '<a> <b>', except swap the first 2 chars of
+    #each string. Assume a and b are length 2 or more.
+    """
 
-    >>> mix_up('mix', 'pod')
-    'pox mid'
-    >>> mix_up('dog', 'dinner')
-    'dig donner'
-    >>> mix_up('gnash', 'sport')
-    'spash gnort'
-    >>> mix_up('pezzy', 'firm')
-    'fizzy perm'
-    """
-    raise NotImplementedError
+mix_up('mix', 'pod')
+
+mix_up('dog', 'dinner')
+
+mix_up('gnash', 'sport')
+
+mix_up('pezzy', 'firm')
 
 
 def verbing(s):
-    """
-    Given a string, if its length is at least 3, add 'ing' to its end.
-    Unless it already ends in 'ing', in which case add 'ly' instead.
-    If the string length is less than 3, leave it unchanged. Return
-    the resulting string.
+    if len(s) >= 3:
+        if s[-3:] == "ing":
+            print s + "ly"
+        else:
+            print s + "ing"
+    else:
+        print s
 
-    >>> verbing('hail')
-    'hailing'
-    >>> verbing('swiming')
-    'swimingly'
-    >>> verbing('do')
-    'do'
     """
-    raise NotImplementedError
+    #Given a string, if its length is at least 3, add 'ing' to its end.
+    #Unless it already ends in 'ing', in which case add 'ly' instead.
+    #If the string length is less than 3, leave it unchanged. Return
+    #the resulting string.
+    """
+
+verbing('hail')
+
+verbing('swiming')
+
+verbing('do')
 
 
 def not_bad(s):
-    """
-    Given a string, find the first appearance of the substring 'not'
-    and 'bad'. If the 'bad' follows the 'not', replace the whole
-    'not'...'bad' substring with 'good'. Return the resulting string.
-    So 'This dinner is not that bad!' yields: 'This dinner is
-    good!'
+    not_loc = s.find("not")
+    bad_loc = s.find("bad")
+    if int(not_loc) == -1:
+        print s
+    elif int(not_loc) < int(bad_loc):
+        print s[:not_loc]+ "good"+ s[bad_loc+3:]
+    else:
+        print s
 
-    >>> not_bad('This movie is not so bad')
-    'This movie is good'
-    >>> not_bad('This dinner is not that bad!')
-    'This dinner is good!'
-    >>> not_bad('This tea is not hot')
-    'This tea is not hot'
-    >>> not_bad("It's bad yet not")
-    "It's bad yet not"
     """
-    raise NotImplementedError
+    #Given a string, find the first appearance of the substring 'not'
+    #and 'bad'. If the 'bad' follows the 'not', replace the whole
+    #'not'...'bad' substring with 'good'. Return the resulting string.
+    #So 'This dinner is not that bad!' yields: 'This dinner is
+    #good!'
+    """
 
+not_bad('This movie is not so bad')
+
+not_bad('This dinner is not that bad!')
+
+not_bad('This tea is not hot')
+
+not_bad("It's bad yet not")
 
 def front_back(a, b):
+    a_length = len(a)
+    b_length = len(b)
+    total_length = int(a_length)+int(b_length)
+    if a_length%2 == 0:
+        a_front = a[:int(a_length)/2]
+        a_back = a[-int(a_length)/2:]
+    else:
+        a_front = a[:(int(a_length)/2)+1]
+        a_back = a[-(int(a_length)/2):]
+    if b_length%2 == 0:
+        b_front = b[:int(b_length)/2]
+        b_back = b[-int(b_length)/2:]
+    else:
+        b_front = b[:(int(b_length)/2)+1]
+        b_back = b[-(int(b_length)/2):]
+    print a_front + b_front + a_back + b_back
+
+
     """
     Consider dividing a string into two halves. If the length is even,
     the front and back halves are the same length. If the length is
@@ -122,12 +170,10 @@ def front_back(a, b):
     'abcde', the front half is 'abc', the back half 'de'. Given 2
     strings, a and b, return a string of the form a-front + b-front +
     a-back + b-back
-
-    >>> front_back('abcd', 'xy')
-    'abxcdy'
-    >>> front_back('abcde', 'xyz')
-    'abcxydez'
-    >>> front_back('Kitten', 'Donut')
-    'KitDontenut'
     """
-    raise NotImplementedError
+
+front_back('abcd', 'xy')
+
+front_back('abcde', 'xyz')
+
+front_back('Kitten', 'Donut')
